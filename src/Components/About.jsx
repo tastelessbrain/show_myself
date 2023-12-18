a738d01c741394b6a5e5bdb60c71fa74ab32bbb8
Mario Breunig <Mario-Breunig@outlook.de>
2023-12-18 19:10:48 +0000

    Just some minor changes still a lot to do.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Student. I'm a IT Specialist. A Develloper. A Designer!";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,10 +32,9 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Python",
+  "Proxmox",
+  "System Admin",
   "Graphic design",
 ];
 
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "If you can set your mind to it and you have some patience, you can achieve it! ~ Someone Smart";
 
 const About = () => {
   return (
